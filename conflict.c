b340d945106f6181bffa8176778eb8b804687b78
Joel Iventosch <joel.iven@ivenapps.com>
2015-03-25 18:20:16 -0500

    changed variable "a" to "b"
    changed variable "a" to "b"...changed variable "a" to "b"

--- conflict.c
+++ conflict.c
@@ -2,13 +2,13 @@
 
 // a silly main function
 int main(int argc, char *argv[]) {
-	int i,a; // rename one of these
+	int i,b; // rename one of these
 
 	// set a to be the average of the 1st 10 numbers
-	a = 0;
+	b = 0;
 	for(i = 0; i < 10; ++i) {
-		a += i;
+		b += i;
 	}
 
-	printf("the average of the 1st 10 numbers is: %d", a/10);
+	printf("the average of the 1st 10 numbers is: %d", b/10);
 }
