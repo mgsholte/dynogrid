8e48c1aef60f0724cbbfeea3394c779aef50fc93
Mark Sholte (laptop.work) <mgsholte@gmail.com>
2015-03-25 18:21:04 -0500

    this will cause a conflict

--- conflict.c
+++ conflict.c
@@ -2,13 +2,13 @@
 
 // a silly main function
 int main(int argc, char *argv[]) {
-	int i,a; // rename one of these
+	int i,c; // rename one of these
 
 	// set a to be the average of the 1st 10 numbers
-	a = 0;
+	c = 0;
 	for(i = 0; i < 10; ++i) {
-		a += i;
+		c += i;
 	}
 
-	printf("the average of the 1st 10 numbers is: %d", a/10);
+	printf("the average of the 1st 10 numbers is: %d", c/10);
 }
