b4485ad8e868ee62959fef74862340d66393f00e
Eggrenade <g361154@trbvm.com>
2015-03-25 18:28:02 -0500

    Added defs.c
    definition of particle struct

---
+++ defs.c
@@ -0,0 +1,10 @@
+//Definitions go here to avoid clutter elsewhere
+
+// Define the particle structure
+
+typedef struct{
+    double pos[2];	//position
+    double p[3];	//momentum
+    double mass;
+    double charge;
+}particle;
