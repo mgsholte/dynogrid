3ca838f2fc727cc44f6affd7b7fa8aa524ead9f9
Mark Sholte (laptop.work) <mgsholte@gmail.com>
2015-03-28 16:48:34 -0500

    created list_add func

--- list.c
+++ list.c
@@ -6,3 +6,11 @@ List list_init(int size) {
 
 	return (List) { size, sentinel };
 }
+
+void list_add(List list, void *payload) {
+	Node *new_node = (Node *)malloc(sizeof(Node));
+
+	new_node->payload = payload;
+	new_node->next = (list.sentinel)->next;
+	(list.sentinel)->next = new_node;
+}
