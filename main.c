370aba503c9881f3cc362bc4b2dd9f0636220ec0
Mark Sholte (laptop.work) <mgsholte@gmail.com>
2015-03-28 16:12:38 -0500

    minor edits to main. removed superfluous makefile

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
+#include <stdio.h>
+
 #include "decs.h"
 #include "grid.h"
 
 int main(int argc, char *argv[]) {
+	int i;  // loop index variable
+
 	// read as inputs in the future
 	int nSteps = 1000;
 	int nx = 100, ny = 100;
 	int part_per_cell = 10;
 	int output_freq = nSteps/10;
 
+	printf("initializing grid and particles");
+
 	double **grid_points = init_grid(nx, ny);
 	particle *particles = init_particles(part_per_cell);
 
-	int i;
+	printf("beginning simulation");
+
 	for(i = 0; i < nSteps; ++i) {
 		time = i*dt;
 		push_particles(grid_points, particles);
@@ -21,4 +28,8 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	output_grid(i, grid_points, particles);
+
+	printf("simulation finished");
+
+	return 0;
 }
