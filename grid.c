2731947933b065f15687a3f1b7e94649194a1ff3
maxdp <maxdp@utexas.edu>
2015-04-01 18:24:00 -0500

    Renamed "grid" var to "grid_points" for consistency

--- grid.c
+++ grid.c
@@ -7,17 +7,18 @@ static double rand_float( double low, double high ) {
 	return ( (double)rand() * (high - low) ) / (double)	RAND_MAX + low;
 }
 
+// inits all grid points to 0 in E and B
 grid_point** init_grid(int nx, int ny) {
-	grid_point **grid = (grid_point**) malloc( ny * sizeof(size_t) ); // allocate an array of pointers to rows
+	grid_point **grid_points = (grid_point**) malloc( ny * sizeof(size_t) ); // allocate an array of pointers to rows
 	for (int i = 0; i < ny; ++i) {
-		grid[i] = (grid_point*) malloc( nx * sizeof(grid_point) );  // allocate the row
+		grid_points[i] = (grid_point*) malloc( nx * sizeof(grid_point) );  // allocate the row
 		for (int j = 0; j < nx; j++) {
 			// all grid points are initialized with no field
-			grid[i][j].E = {0,0,0};
-			grid[i][j].B = {0,0,0};
+			grid_points[i][j].E = {0,0,0};
+			grid_points[i][j].B = {0,0,0};
 		}
 	}
-	return grid;
+	return grid_points;
 }
 
 // inits rectangular region of particles, approx evenly distributed
