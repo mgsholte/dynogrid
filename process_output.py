456a2c711da9ab8319e3308bf32db39421d34118
Joel Iventosch <joel.iven@ivenapps.com>
2015-04-01 18:15:19 -0500

    updated visualization files

--- process_output.py
+++ process_output.py
@@ -1,5 +1,61 @@
 # imports:
 import process_output_functions as fn
+import numpy as np
+import scipy as sci
+import scipy.linalg as lin
+import  matplotlib.cm as cm
+import os
 
+# gather inputs for creating "fake" files:
+nx = int(raw_input("nx? \t\t\t"))
+ny = int(raw_input("ny? \t\t\t"))
+nz = int(raw_input("nz? \t\t\t"))
+numParticles = int(raw_input("how many particles? \t"))
+numFiles = int(raw_input("how many files? \t"))
+simID = raw_input("simID? \t")
+dim = -1
+# nx = 100
+# ny = 100
+# nz = 100
+# numParticles = 1000
+# numFiles = 50
+
+print("\n\nCreating fake files...")
 # generate fake output files to test visualization functionality with:
-fn.createTestOutputFiles()
+if nz > 0:
+    dim = 3
+    fn.createTestOutputFiles_ALL3D(nx, ny, nz, numParticles, numFiles)
+else:
+    dim = 2
+    fn.createTestOutputFiles_ALL2D(nx, ny, numParticles, numFiles)
+print("*****************************\n")
+
+# suck in all the gridpoint files and parse the text for the relevant data:
+if(dim == 2):
+    timesteps, E_min, E_max, B_min, B_max = fn.getGridData2D(numFiles)
+elif(dim == 3):
+    timesteps, E_min, E_max, B_min, B_max = fn.getGridData3D(numFiles)
+
+# suck in all the particles files and parse the text for the relevant data:
+if(dim == 2):
+    timesteps, p_min, p_max, = fn.getParticleData2D(numFiles, timesteps)
+elif (dim == 3):
+    timesteps, p_min, p_max, = fn.getParticleData3D(numFiles, timesteps)
+
+# normalize data:
+timesteps = fn.normalizeData(timesteps, E_min, E_max, B_min, B_max, p_min, p_max)
+
+# plot data:
+cur_dir = os.getcwd()
+path_name = "sim_" + str(simID)
+path = os.path.join(cur_dir, path_name)
+
+if(dim == 2):
+    for t in xrange(0, numFiles):
+        fn.plotDataForSingleTimeStep2D(timesteps[t]['gridpoints'], timesteps[t]['particles'], t, nx, ny, path)
+elif(dim == 3):
+    for t in xrange(0, numFiles):
+        fn.plotDataForSingleTimeStep3D(timesteps[t]['gridpoints'], timesteps[t]['particles'], t, nx, ny, nz, path)
+
+   
+            
--- process_output_functions.py
+++ process_output_functions.py
@@ -1,50 +1,391 @@
 # imports:
 import random
+import matplotlib.pyplot as plt
+import os
+import  matplotlib.cm as cm
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
-def createTestOutputFiles():
-    itNum = 0
+
+def createTestOutputFile2D(itNum, nx, ny, numParticles, numFiles):
     dt = 0.000000000000000018
     # create a fake output file for itNum_grid.txt
     f_name = str(itNum) + "_grid.txt"
-    with open(f_name, 'a+') as test_file:
+    with open(f_name, 'w+') as test_file:
         test_file.write("GRIDPOINTS\n")
         test_file.write("itNum=" + str(itNum) + "\n")
         test_file.write("Time=" + str(itNum*dt) + "\n")
         test_file.write("TimeStep=" + str(dt) + "\n")
         test_file.write("GridSize:nx=100,ny=100\n")
         
-        for i in xrange(0,100):
-            for j in xrange(0,100):       
+        for i in xrange(0, ny):
+            for j in xrange(0, nx):       
                 if itNum == 0:
-                    test_file.write("coordinates:"+str(i)+","+str(j)+","+"Ex=0,Ey=0,Ez=0,Bx=0,By=0,Bz=0\n")
+                    test_file.write(str(j)+","+str(i)+","+ "0.0,0.0\n")
                 else:
-                    Ex = random.random()/1000.0
-                    Ey = random.random()/1000.0
-                    Ez = random.random()/1000.0
-                    Bx = random.random()/1000.0
-                    By = random.random()/1000.0
-                    Bz = random.random()/1000.0
-                    test_file.write("coordinates:" + str(i) + "," + str(j) + "," + "Ex=" + str(Ex) + "," + "Ey=" + str(Ey) + "," + "Ez=" + str(Ez) + "," + "Bx=" + str(Bx) + "," + "By=" + str(By) + "," + "Bz=" + str(Bz) + "\n")
+                    E = random.random()/1000.00
+                    B = random.random()/1000.0
+                    test_file.write(str(j) + "," + str(i) + "," + str(E) + "," + str(B) + "\n")
     
     # create a fake output file for itNum_particles.txt
     f_name2 = str(itNum) + "_particles.txt"
-    with open(f_name2, 'a+') as test_file2:
+    with open(f_name2, 'w+') as test_file2:
         test_file2.write("PARTICLES\n")
         test_file2.write("itNum=" + str(itNum) + "\n")
         test_file2.write("Time=" + str(itNum*dt) + "\n")
         test_file2.write("TimeStep=" + str(dt) + "\n")
         test_file2.write("GridSize:nx=100,ny=100\n")
         
-        for i in xrange(0,1000):
+        for i in xrange(0, numParticles):
                 if itNum == 0:
-                    pos_x = (random.random()*10.0)+45.0
-                    pos_y = (random.random()*10.0)+45.0
-                    test_file2.write("pos_x=" + str(pos_x) + "," + "pos_y=" + str(pos_y) + "," + "p_x=0,p_y=0,p_z=0\n")
+                    pos_x = (random.random()*(nx/10.0))+((nx/2.0)-5.0)
+                    pos_y = (random.random()*(ny/10.0))+((ny/2.0)-5.0)
+                    test_file2.write("ptcl:" + str(i) + "," + str(pos_x) + "," + str(pos_y) + "," + "0.0\n")
                 else:
-                    px = random.random()/1000000000.0
-                    py = random.random()/1000000000.0
-                    pz = random.random()/1000000000.0
-                    pos_x = (random.random()*10.0)+45.0
-                    pos_y = (random.random()*10.0)+45.0
-                    test_file2.write("pos_x=" + str(pos_x) + "," + "pos_y=" + str(pos_y) + "," + "p_x=" + str(px) + "," + "p_y=" + str(py) + "," + "p_z=" + str(pz) + "\n")
+                    p = random.random()/1000000000.0
+                    multFac = (float(itNum)/float(numFiles))
+                    if multFac < 0.1:
+                        multFac = 0.1
+                    xrng = nx*multFac
+                    xspread = (nx-xrng)/2.0
+                    yrng = ny*multFac
+                    yspread = (ny-yrng)/2.0
+                    pos_x = (random.uniform(xspread, nx-xspread))
+                    pos_y = (random.uniform(yspread, ny-yspread))
+                    test_file2.write("ptcl:" + str(i) + "," + str(pos_x) + "," + str(pos_y) + "," + str(p) + "\n")
+
+def createTestOutputFile3D(itNum, nx, ny, nz, numParticles, numFiles):
+    dt = 0.000000000000000018
+    # create a fake output file for itNum_grid.txt
+    f_name = str(itNum) + "_grid.txt"
+    with open(f_name, 'w+') as test_file:
+        test_file.write("GRIDPOINTS\n")
+        test_file.write("itNum=" + str(itNum) + "\n")
+        test_file.write("Time=" + str(itNum*dt) + "\n")
+        test_file.write("TimeStep=" + str(dt) + "\n")
+        test_file.write("GridSize:nx=100,ny=100,nz=100\n")
         
+        for i in xrange(0, ny):
+            for j in xrange(0, nx): 
+                for k in xrange(0, nz):      
+                    if itNum == 0:
+                        test_file.write(str(j)+","+ str(i) +"," + str(k) + ",0.0,0.0\n")
+                    else:
+                        E = random.random()/1000.0
+                        B = random.random()/1000.0
+                        test_file.write(str(j) + "," + str(i) + "," + str(k) + "," + str(E) + "," + str(B) + "\n")
+    
+    # create a fake output file for itNum_particles.txt
+    f_name2 = str(itNum) + "_particles.txt"
+    with open(f_name2, 'w+') as test_file2:
+        test_file2.write("PARTICLES\n")
+        test_file2.write("itNum=" + str(itNum) + "\n")
+        test_file2.write("Time=" + str(itNum*dt) + "\n")
+        test_file2.write("TimeStep=" + str(dt) + "\n")
+        test_file2.write("GridSize:nx=100,ny=100\n")
+        
+        for i in xrange(0, numParticles):
+                if itNum == 0:
+                    pos_x = (random.random()*(nx/10.0))+((nx/2.0)-5.0)
+                    pos_y = (random.random()*(ny/10.0))+((ny/2.0)-5.0)
+                    pos_z = (random.random()*(nz/10.0))+((nz/2.0)-5.0)
+                    test_file2.write("ptcl:" + str(i) + "," + str(pos_x) + "," + str(pos_y) + "," + str(pos_z) + ",0.0\n")
+                else:
+                    p = random.random()/1000000000.0
+                    multFac = (float(itNum)/float(numFiles))
+                    if multFac < 0.1:
+                        multFac = 0.1
+                    xrng = nx*multFac
+                    xspread = (nx-xrng)/2.0
+                    yrng = ny*multFac
+                    yspread = (ny-yrng)/2.0
+                    zrng = nz*multFac
+                    zspread = (nz-zrng)/2.0
+                    pos_x = (random.uniform(xspread, nx-xspread))
+                    pos_y = (random.uniform(yspread, ny-yspread))
+                    pos_z = (random.uniform(zspread, nz-zspread))
+                    test_file2.write("ptcl:" + str(i) + "," + str(pos_x) + "," + str(pos_y) + "," + str(pos_z) + "," + str(p) + "\n")
+
+
+def createTestOutputFiles_ALL2D(nx, ny, numParticles, numFiles):
+    for i in xrange(0, numFiles):
+        createTestOutputFile2D(i, nx, ny, numParticles, numFiles)
+        
+def createTestOutputFiles_ALL3D(nx, ny, nz, numParticles, numFiles):
+    for i in xrange(0, numFiles):
+        createTestOutputFile3D(i, nx, ny, nz, numParticles, numFiles)        
+
+def getGridData2D(numFiles):   
+    timesteps = list()
+    # suck in all the gridpoint files and parse the text for the relevant data:
+    E_min = 0.0
+    E_max= 0.0
+    B_min = 0.0
+    B_max = 0.0
+    fname_grid = "_grid.txt"
+    for i in xrange(0, numFiles):
+        fname = str(i) + str(fname_grid)
+        gridpoints = dict()
+        Xs = list()
+        Ys = list()
+        Es = list()
+        Bs = list()
+        with open(fname, 'r') as grid_file:
+            grid_file_lines = grid_file.readlines() # grid_file_lines is a list of the lines in grid_file
+            line_ct = 0
+            for line in grid_file_lines:
+                line_ct += 1
+                if line_ct <= 5:
+                    continue
+                # parse eachline to get needed data:
+                eachline = line.split(',') # eachline is a list of the results of the split
+                xcoord = int(eachline[0])
+                ycoord = int(eachline[1])
+                E = float(eachline[2])
+                B = float(eachline[3])
+                if E < E_min:
+                    E_min = E
+                if E > E_max:
+                    E_max = E
+                if B < B_min:
+                    B_min = B
+                if B > B_max:
+                    B_max = B
+                Xs.append(xcoord)
+                Ys.append(ycoord)
+                Es.append(E)
+                Bs.append(B)
+        # store data for gridpoints in a dictionary:
+        gridpoints = {'Xs':Xs, 'Ys':Ys, 'Es':Es, 'Bs':Bs}
+        timesteps.append({'gridpoints':gridpoints,'particles':''})
+    return timesteps, E_min, E_max, B_min, B_max
+
+def getParticleData2D(numFiles, timesteps):
+    p_min = 0.0
+    p_max = 0.0        
+    # suck in all the particles files and parse the text for the relevant data:            
+    fname_particles = "_particles.txt"
+    for i in xrange(0, numFiles):
+        fname = str(i) + str(fname_particles)
+        particles = dict()
+        Xs = list()
+        Ys = list()
+        ps = list()
+        with open(fname, 'r') as particle_file:
+            particles = list()
+            particle_file_lines = particle_file.readlines() # grid_file_lines is a list of the lines in grid_file
+            line_ct = 0
+            for line in particle_file_lines:
+                line_ct += 1
+                if line_ct <= 5:
+                    continue
+                # parse eachline to get needed data:
+                eachline = line.split(',') # eachline is a list of the results of the split
+                xcoord = float(eachline[1])
+                ycoord = float(eachline[2])
+                p = float(eachline[3])
+                if p < p_min:
+                    p_min = p
+                if p > p_max:
+                    p_max = p
+                # store data for in a dictionary:
+                Xs.append(xcoord)
+                Ys.append(ycoord)
+                ps.append(p)
+        particles = {'Xs':Xs, 'Ys':Ys, 'ps':ps}
+        (timesteps[i])['particles'] = particles
+    return timesteps, p_min, p_max
+
+
+def getGridData3D(numFiles):   
+    timesteps = list()
+    # suck in all the gridpoint files and parse the text for the relevant data:
+    E_min = 0.0
+    E_max= 0.0
+    B_min = 0.0
+    B_max = 0.0
+    fname_grid = "_grid.txt"
+    for i in xrange(0, numFiles):
+        fname = str(i) + str(fname_grid)
+        gridpoints = dict()
+        Xs = list()
+        Ys = list()
+        Zs = list()
+        Es = list()
+        Bs = list()
+        with open(fname, 'r') as grid_file:
+            grid_file_lines = grid_file.readlines() # grid_file_lines is a list of the lines in grid_file
+            line_ct = 0
+            for line in grid_file_lines:
+                line_ct += 1
+                if line_ct <= 5:
+                    continue
+                # parse eachline to get needed data:
+                eachline = line.split(',') # eachline is a list of the results of the split
+                xcoord = int(eachline[0])
+                ycoord = int(eachline[1])
+                zcoord = int(eachline[2])
+                E = float(eachline[3])
+                B = float(eachline[4])
+                if E < E_min:
+                    E_min = E
+                if E > E_max:
+                    E_max = E
+                if B < B_min:
+                    B_min = B
+                if B > B_max:
+                    B_max = B
+                Xs.append(xcoord)
+                Ys.append(ycoord)
+                Zs.append(zcoord)
+                Es.append(E)
+                Bs.append(B)
+                # store data in a dictionary:
+        gridpoints = {'Xs':Xs, 'Ys':Ys, 'Zs':Zs, 'Es':Es, 'Bs':Bs}
+        timesteps.append({'gridpoints':gridpoints,'particles':''})
+    return timesteps, E_min, E_max, B_min, B_max
+        
+def getParticleData3D(numFiles, timesteps):
+    p_min = 0.0
+    p_max = 0.0        
+    # suck in all the particles files and parse the text for the relevant data:            
+    fname_particles = "_particles.txt"
+    for i in xrange(0, numFiles):
+        fname = str(i) + str(fname_particles)
+        particles = dict()
+        Xs = list()
+        Ys = list()
+        Zs = list()
+        ps = list()
+        with open(fname, 'r') as particle_file:
+            particles = list()
+            particle_file_lines = particle_file.readlines() # grid_file_lines is a list of the lines in grid_file
+            line_ct = 0
+            for line in particle_file_lines:
+                line_ct += 1
+                if line_ct <= 5:
+                    continue
+                # parse eachline to get needed data:
+                eachline = line.split(',') # eachline is a list of the results of the split
+                xcoord = float(eachline[1])
+                ycoord = float(eachline[2])
+                zcoord = float(eachline[3])
+                p = float(eachline[4])
+                if p < p_min:
+                    p_min = p
+                if p > p_max:
+                    p_max = p
+                # store data in a dictionary:
+                Xs.append(xcoord)
+                Ys.append(ycoord)
+                Zs.append(zcoord)
+                ps.append(p)
+        particles = {'Xs':Xs, 'Ys':Ys, 'Zs':Zs, 'ps':ps}
+        (timesteps[i])['particles'] = particles
+    return timesteps, p_min, p_max
+        
+        
+def normalizeData(timesteps, E_min, E_max, B_min, B_max, p_min, p_max):
+    # (x-min)/(max-min)
+    for t in xrange(0, len(timesteps)):
+        # normalize gridpoint data:
+        for grdpt in xrange(0, len(((timesteps[t])['gridpoints'])['Es'])):
+            # normalize E and B in each gridpoint dictionary:
+            if E_min == E_max:
+                (((timesteps[t])['gridpoints'])['Es'])[grdpt] = 0.5
+            else:
+                E_temp = (((timesteps[t])['gridpoints'])['Es'])[grdpt]
+                (((timesteps[t])['gridpoints'])['Es'])[grdpt] = ((E_temp-E_min)/(E_max-E_min))
+            if B_min == B_max:
+                (((timesteps[t])['gridpoints'])['Bs'])[grdpt] = 0.5
+            else:
+                B_temp = (((timesteps[t])['gridpoints'])['Bs'])[grdpt]
+                (((timesteps[t])['gridpoints'])['Bs'])[grdpt] = ((B_temp-B_min)/(B_max-B_min))
+        # normalize particle data:        
+        for ptcl in xrange(0, len(((timesteps[t])['particles'])['ps'])):
+            # normalize p in each particle dictionary:
+            if p_min == p_max:
+                (((timesteps[t])['particles'])['ps'])[ptcl] = 0.5
+            else:
+                p_temp = (((timesteps[t])['particles'])['ps'])[ptcl]
+                (((timesteps[t])['particles'])['ps'])[ptcl] = ((p_temp-p_min)/(p_max-p_min))
+    
+    return timesteps
+
+
+
+def plotDataForSingleTimeStep2D(gridpoints, particles, itNum, nx, ny, path):
+    fig = plt.figure(figsize=(12,7))
+    ax1 = fig.add_subplot(111)
+    gridpoints_Xs = np.array(gridpoints['Xs'])
+    gridpoints_Ys = np.array(gridpoints['Ys'])
+    gridpoints_Es = np.array(gridpoints['Es'])
+    gridpoints_Bs = np.array(gridpoints['Bs'])
+    itNum = '{0:05d}'.format(itNum)
+    title = "B_field_" + str(itNum)
+    fig.suptitle(title, fontsize='20')
+    SM = cm.ScalarMappable()
+    cMap = SM.get_cmap()
+    plt.xlabel('X', fontsize=16)
+    plt.ylabel('Y', fontsize=16)
+    plt.xlim(0, nx)
+    plt.ylim(0, ny)
+    cb1 = ax1.scatter(gridpoints_Xs, gridpoints_Ys, s=40, c=gridpoints_Bs, marker=u'o', cmap='binary', linewidths=0, alpha=.3, label='B Field')
+
+    particles_Xs = np.array(particles['Xs'])
+    particles_Ys = np.array(particles['Ys'])
+    particles_ps = np.array(particles['ps'])
+    cb2 = ax1.scatter(particles_Xs, particles_Ys, c=particles_ps, marker=u'o', cmap='coolwarm', linewidths=.3, label='Particles, mapped by momentum')
+
+    legend = ax1.legend(loc='upper right', shadow=True)
+    plt.colorbar(cb1)
+    plt.colorbar(cb2)
+#     plt.show()
+    if os.access(path, os.F_OK):
+        os.chdir(path)
+    else:
+        os.mkdir(path)
+        os.chdir(path)
+    fig.savefig(title)
+    
+    
+def plotDataForSingleTimeStep3D(gridpoints, particles, itNum, nx, ny, nz, path):
+    fig = plt.figure(figsize=(12,7))
+    ax1 = fig.add_subplot(111, projection='3d')
+    gridpoints_Xs = np.array(gridpoints['Xs'])
+    gridpoints_Ys = np.array(gridpoints['Ys'])
+    gridpoints_Zs = np.array(gridpoints['Zs'])
+    gridpoints_Es = np.array(gridpoints['Es'])
+    gridpoints_Bs = np.array(gridpoints['Bs'])
+    itNum = '{0:05d}'.format(itNum)
+    title = "B_field_" + str(itNum)
+    fig.suptitle(title, fontsize='20')
+    SM = cm.ScalarMappable()
+    cMap = SM.get_cmap()
+                        
+    ax1.set_xlabel('X', fontsize=16)
+    ax1.set_ylabel('Y', fontsize=16)
+    ax1.set_zlabel('Z', fontsize=16)
+    ax1.set_xlim(0, nx)
+    ax1.set_ylim(0, ny)
+    ax1.set_zlim(0, nz)
+    cb1 = ax1.scatter(gridpoints_Xs, gridpoints_Ys, gridpoints_Zs, s=40, c=gridpoints_Bs, marker=u'o', cmap='binary', linewidths=0, alpha=.1, label='B Field')
+
+    particles_Xs = np.array(particles['Xs'])
+    particles_Ys = np.array(particles['Ys'])
+    particles_Zs = np.array(particles['Zs'])
+    particles_ps = np.array(particles['ps'])
+    cb2 = ax1.scatter(particles_Xs, particles_Ys, particles_Zs, c=particles_ps, marker=u'o', cmap='coolwarm', linewidths=.3, label='Particles, mapped by momentum')
+   
+    legend = ax1.legend(loc='upper right', shadow=True)
+    plt.colorbar(cb1)
+    plt.colorbar(cb2)
+#     plt.show()
+    if os.access(path, os.F_OK):
+        os.chdir(path)
+    else:
+        os.mkdir(path)
+        os.chdir(path)
+    fig.savefig(title)
+    
+    
