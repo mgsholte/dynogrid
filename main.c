13dda2cf618b2f391a8991a8231b0a64b381ad4f
Mark Sholte (laptop.work) <mgsholte@gmail.com>
2015-04-12 19:18:04 -0500

    fixed possible double print bug

--- main.c
+++ main.c
@@ -12,8 +12,9 @@ int main(int argc, char *argv[]) {
 	int nSteps = ceil(dt/t_end);
 	//int nx = 100, ny = 100, nz = 100;
 	int part_per_cell = 5;
+	// print output every output_freq iterations of the main loop
 	int output_freq = nSteps/10;
-	//vec2 ul = {45, 45}, lr = {55, 55};  // upper left, lower right coordinates defining the rectangle where particles begin in the simulation
+	// upper left coordinate and dimensions defining the rectangle where particles begin in the simulation
 	vec3 ulf = {.46*x_max, .46*y_max, .46*z_max};
 	vec3 dims = {.08*x_max, .08*y_max, .08*z_max};
 
@@ -27,13 +28,16 @@ int main(int argc, char *argv[]) {
 	for(i = 0; i < nSteps; ++i) {
 		time = i*dt;
 		push_particles(grid_points, particles);
-		update_grid(grid_points);  // add the laser, ...
+		update_grid(grid_points);  // add the laser, etc.
 		if (i % output_freq == 0) {
 			output_grid((i/output_freq), (nSteps/output_freq), grid_points, particles);
 		}
 	}
-	output_grid((i/output_freq), (nSteps/output_freq), grid_points, particles);
 
+	// print final state unless it was already output on the last iteration of the above loop
+	if (i % output_freq != 0)
+		output_grid((i/output_freq), (nSteps/output_freq), grid_points, particles);
+	 
 	printf("simulation finished\n");
 
 	return 0;
