fd11adbba2c35fefc07f45e496e43d61308146c2
Joel Iventosch <joeliven@login1.ls4.tacc.utexas.edu>
2015-05-02 18:15:20 -0500

    updates to grid.c and grid.h; changed init_particles() function to parallel distributed implementation

--- grid.h
+++ grid.h
@@ -7,8 +7,8 @@
 // return the initialized grid. make a grid with 'nx' points along x, 'ny' points along y, and 'nz' points along z. these 3 parameters are global variables in decs.h
 grid_cell*** init_grid();
 
-// return the list of particles. put 'part_per_cell' particles in each grid cell within the rectangular prism with the specified origin and dimensions. the origin is the upper-left-front-most point of the prism
-List init_particles(vec3 origin, vec3 dims, int part_per_cell);
+// Populate each cell with its list of particles. Put 'part_per_cell' particles in each grid cell within the rectangular prism with the specified origin and dimensions. the origin is the upper-left-front-most point of the prism
+void init_particles(grid_cell**** grid_cells, vec3 origin, vec3 dims, int part_per_cell);
 
 bool coarsen(grid_cell *cell);
 bool refine(grid_cell* cell, double x_spat, double y_spat, double z_spat, vec3 *h);
