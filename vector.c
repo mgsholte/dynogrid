9fe67a40723f1ca500e0f83394251eb786b5c7c7
Mark Sholte (laptop.work) <mgsholte@gmail.com>
2015-05-04 16:19:15 -0500

    separate vector class
    moved vec3 declaration to its own module. added some functions that operate on vec3s

---
+++ vector.c
@@ -0,0 +1,9 @@
+#include "vector.h"
+
+void vec3_scale(vec3 *v, double factor) {
+	v->x *= factor;	v->y *= factor;	v->z *= factor;
+}
+
+void vec3_add(vec3 *v, vec3* w) {
+	v->x += w->x; v->y += w->y; v->z += w->z;
+}
---
+++ vector.h
@@ -0,0 +1,20 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+// a 2D vector
+typedef struct {
+	double x,y;
+} vec2;
+
+// a 3D vector
+typedef struct {
+	double x,y,z;
+} vec3;
+
+// scale the vector v (in-place) by the scalar factor
+void vec3_scale(vec3 *v, double factor);
+
+// add the vectors v,w and store result in v
+void vec3_add(vec3 *v, vec3* w);
+
+#endif //VECTOR_H
