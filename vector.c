9531d4dc68983a37d552504fc6167c3b5fd0894c
Mark Sholte (laptop.work) <mgsholte@gmail.com>
2015-05-04 16:43:17 -0500

    vec3_add takes one less pointer arg

--- vector.c
+++ vector.c
@@ -4,6 +4,6 @@ void vec3_scale(vec3 *v, double factor) {
 	v->x *= factor;	v->y *= factor;	v->z *= factor;
 }
 
-void vec3_add(vec3 *v, vec3* w) {
-	v->x += w->x; v->y += w->y; v->z += w->z;
+void vec3_add(vec3 *v, vec3 w) {
+	v->x += w.x; v->y += w.y; v->z += w.z;
 }
