b767d3b5dfdf33d2b91300a7618ddc204ee9fffd
Eggrenade <g361154@trbvm.com>
2015-05-10 14:44:03 -0500

    Push updates for pxmin

--- push.c
+++ push.c
@@ -105,9 +105,9 @@ static void push_one_cell(tree ****grid, List part_list) {
 		//Do interpolation to find e and b here.
         // x-left, y-up, and z-near indices
 		// Subtract out the local min to get the correct indicies
-        xl = floor(((curr->pos).x - pxmin) * idx) + imin + 1;
-        yu = floor(((curr->pos).y - pymin) * idy) + jmin + 1;
-		zn = floor(((curr->pos).z - pzmin) * idz) + kmin + 1;
+        xl = floor(((curr->pos).x - pxmin) * idx) + imin;
+        yu = floor(((curr->pos).y - pymin) * idy) + jmin;
+		zn = floor(((curr->pos).z - pzmin) * idz) + kmin;
 		// x-right fraction, ...
 		// This stays the same for parallel, I think
         xrf = ((curr->pos).x - xl*dx) / dx;
@@ -118,7 +118,7 @@ static void push_one_cell(tree ****grid, List part_list) {
 
 		TreeNode *cellIter = grid[xl][yu][zn]->root;
 		//Find the finest cell that contains the particle
-		while (cellIter->children != NULL) {
+		while (cellIter->children[i] != NULL) {
 			if (xrf < .5){
 				if (ydf < .5){
 					if (zff < .5){
@@ -236,9 +236,9 @@ static void push_one_cell(tree ****grid, List part_list) {
 		// Pass the particles to neighbor cells if necessary
         // Ending x-left, y-up, and z-near indices
 		// Subtract out the local min to get the correct indicies
-        xle = floor(((curr->pos).x - pxmin) * idx) + imin + 1;
-        yue = floor(((curr->pos).y - pymin) * idy) + jmin + 1;
-		zne = floor(((curr->pos).z - pzmin) * idz) + kmin + 1;
+        xle = floor(((curr->pos).x - pxmin) * idx) + imin;
+        yue = floor(((curr->pos).y - pymin) * idy) + jmin;
+		zne = floor(((curr->pos).z - pzmin) * idz) + kmin;
 
 		// Check if cell has changed
 		// Guarenteed to still be in a cell or ghost cell controled by proc
@@ -253,9 +253,9 @@ static void push_one_cell(tree ****grid, List part_list) {
 // Finds the list that these particles belong in
 List* part_belongs_in(tree ****grid, vec3 pos) {
 	int i,j,k;
-    i = floor((pos.x - pxmin) / dx) + imin + 1;
-    j = floor((pos.y - pymin) / dy) + jmin + 1;
-	k = floor((pos.z - pzmin) / dz) + kmin + 1;
+    i = floor((pos.x - pxmin) / dx) + imin;
+    j = floor((pos.y - pymin) / dy) + jmin;
+	k = floor((pos.z - pzmin) / dz) + kmin;
 	return &(grid[i][j][k]->particles);
 }
 
